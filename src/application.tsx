69d1dfc235f94beda7192824a965355cb7bbdc12
Anya Tokar <61166946+anyatokar@users.noreply.github.com>
2021-01-26 23:42:12 -0500

    connected with firestone to show buildings

--- src/application.tsx
+++ src/application.tsx
@@ -1,12 +1,43 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Route, Switch, RouteComponentProps } from 'react-router-dom';
 import logging from './config/logging';
 import routes from './config/routes';
+import firebase from './firebase';
+import 'firebase/firestore';
+import { Table } from 'react-bootstrap';
 
 const Application: React.FunctionComponent<{}> = props => {
+
+    const [buildings, setBuildings] = useState([] as any);
+    const [loading, setLoading] = useState(false);
+
+    const ref = firebase.firestore().collection("buildings");
+
+    function getBuildings() {
+        setLoading(true);
+        ref.onSnapshot((querySnapshot) => {
+            const items: Array<object> = [];
+            querySnapshot.forEach((doc) => {
+                items.push(doc.data());
+            });
+            setBuildings(items)
+            setLoading(false)
+
+        });
+    }
+
     useEffect(() => {
-        logging.info('Loading application.');
-    }, [])
+        getBuildings();
+    }, []);
+
+    if (loading) {
+        return <h1>loading...</h1>;
+    }
+
+    // useEffect(() => {
+    //     logging.info('Loading application.');
+    // }, [])
+    console.log(buildings)
 
     return (
         <div>
@@ -30,6 +61,20 @@ const Application: React.FunctionComponent<{}> = props => {
                     })}
                 </Switch>
             </BrowserRouter>
+
+            <h1>Buildings</h1>
+            {buildings.map((building: any) => (
+                <div key={building.id}>
+                    <h2>{building.building_name}</h2>
+                    <h2>{building.address}</h2>
+                    <h2>{building.phone}</h2>
+                    <h2>{building.residential_targeted_area}</h2>
+                    <h2>{building.total_restricted_units}</h2>
+
+                </div>
+            ))}
+
+
         </div>
     );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './application';
+import Application from './application';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Application />
   </React.StrictMode>,
   document.getElementById('root')
 );
