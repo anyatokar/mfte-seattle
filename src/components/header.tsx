64e217258d01886bb72299fd909cc3d1d82e9a61
Anya Tokar <61166946+anyatokar@users.noreply.github.com>
2021-02-03 18:28:20 -0500

    bootstrap

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,14 +3,20 @@ import {LinkContainer} from 'react-router-bootstrap';
 
 export const Header = () => {
   return (
-    <div>
-      <Navbar bg="light" variant="light">
+    <div className="navbar">
+      <Navbar variant="light">
         <Nav className="mr-auto">
           <LinkContainer to='/'>
             <Nav.Link><strong>MFTE Simple</strong></Nav.Link>
           </LinkContainer>
           <LinkContainer to='/buildings'>
-            <Nav.Link>Properties</Nav.Link>
+            <Nav.Link>All Buildings</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to='/about-mfte'>
+            <Nav.Link>About MFTE</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to='/about-mfte-simple'>
+            <Nav.Link>About this app</Nav.Link>
           </LinkContainer>
           {/* <LinkContainer to='./pages/about'>
             <Nav.Link>About</Nav.Link>
