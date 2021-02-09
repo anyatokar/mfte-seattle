f623bb02318677aeb37c0e91ce3dd6e34d416ba0
Anya Tokar <61166946+anyatokar@users.noreply.github.com>
2021-02-08 17:20:07 -0800

    auth working but no limited to certain parts of app yet

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,19 @@
 import React, { useEffect } from 'react';
 import { Header } from './components/header';
 import { Footer } from './components/footer';
-import { BrowserRouter, Route, Switch, RouteComponentProps } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, RouteComponentProps } from 'react-router-dom';
 import logging from './config/logging';
 import routes from './config/routes';
+
+import Signup from "./auth_components/Signup"
+import { Container } from "react-bootstrap"
+import { AuthProvider } from "./contexts/AuthContext"
+import Dashboard from "./auth_components/Dashboard"
+import Login from "./auth_components/Login"
+import PrivateRoute from "./auth_components/PrivateRoute"
+import ForgotPassword from "./auth_components/ForgotPassword"
+import UpdateProfile from "./auth_components/UpdateProfile"
+
 
 
 
@@ -16,7 +26,7 @@ const Application: React.FunctionComponent<{}> = props => {
   return (
     <div>
 
-      <BrowserRouter>
+      {/* <Router>
       <Header />
 
         <Switch>
@@ -37,7 +47,41 @@ const Application: React.FunctionComponent<{}> = props => {
             );
           })}
         </Switch>
-      </BrowserRouter>
+      </Router> */}
+
+
+      <div>
+        <Router>
+          
+          <AuthProvider>
+            <Switch>
+              <PrivateRoute exact path="/" component={Dashboard} />
+              <PrivateRoute path="/update-profile" component={UpdateProfile} />
+              <Route path="/signup" component={Signup} />
+              <Route path="/login" component={Login} />
+              <Route path="/forgot-password" component={ForgotPassword} />
+              
+              {routes.map((route, index) => {
+            return (
+              <Route 
+                key={index}
+                path={route.path}
+                exact={route.exact}
+                render={(props: RouteComponentProps<any>) => (
+                  <route.component
+                    name={route.name} 
+                    {...props}
+                    {...route.props}
+                  />
+                )}
+              />
+            );
+          })}
+
+            </Switch>
+          </AuthProvider>
+        </Router>
+      </div>
 
       <Footer />
     </div>
