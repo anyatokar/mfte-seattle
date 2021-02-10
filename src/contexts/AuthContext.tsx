5fd98fb914068ffc0a8b2ca7ac1a48a1878f9dda
Anya Tokar <61166946+anyatokar@users.noreply.github.com>
2021-02-10 14:37:26 -0800

    when user authenticated a document is generated in the users collection under their id

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState, useEffect, createContext } from "react"
 import { auth } from "../db/firebase"
 import IProps from "../interfaces/IProps"
+import firebase from "../db/firebase"
 
 const AuthContext = createContext({})
 
@@ -12,8 +13,20 @@ export function AuthProvider({ children}: IProps) {
   const [currentUser, setCurrentUser] = useState() as any
   const [loading, setLoading] = useState(true)
 
-  function signup(email: string, password: string) {
-    return auth.createUserWithEmailAndPassword(email, password)
+  // function signup(email: string, password: string) {
+  //   return auth.createUserWithEmailAndPassword(email, password)
+  // }
+
+  async function signup(email: string, password: string) {
+    const user = firebase.auth().createUserWithEmailAndPassword(email, 
+      password).then(cred => {
+        if (cred.user) {
+      return firebase.firestore().collection('users').doc(cred.user.uid).set({
+        savedHomes: {}, 
+        savedSearches: {}
+      })
+      }})
+      return { user }
   }
 
   function login(email: string, password: string) {
