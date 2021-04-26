61a624db7b10d9b1c6e57e870bdb57e419df206b
Anya Tokar <61166946+anyatokar@users.noreply.github.com>
2021-04-25 22:44:52 -0700

    tiny: added semicolons & fixed spacing & removed unused className

--- src/components/AllBuildingsCard.tsx
+++ src/components/AllBuildingsCard.tsx
@@ -1,13 +1,12 @@
 import { useState, useContext } from "react";
 import IBuilding from "../interfaces/IBuilding";
-import firebase from "../db/firebase"
+import firebase from "../db/firebase";
 import { useAuth } from "../contexts/AuthContext";
 import { ModalContext, ModalState } from "../contexts/ModalContext";
 import { Card, Button, ListGroup, ListGroupItem } from 'react-bootstrap';
 
 export function AllBuildingsCard(props: IBuilding) {
-
-  const { currentUser } = useAuth() as any
+  const {currentUser} = useAuth() as any;
   const {
     buildingID,
     buildingName,
@@ -30,12 +29,12 @@ export function AllBuildingsCard(props: IBuilding) {
     lng
   } = props;
 
-  const [buttonFill, setButtonFill] = useState(false) as any
+  const [buttonFill, setButtonFill] = useState(false) as any;
 
   function saveBuilding(e: any) {
-    setButtonFill(true)
-    firebase.firestore().collection("users").doc(currentUser.uid).collection("savedHomes").doc(buildingID).set(
-      {
+    setButtonFill(true);
+    firebase.firestore().collection("users").doc(currentUser.uid).collection("savedHomes").doc(buildingID)
+    .set({
         "buildingID": buildingID,
         "buildingName": buildingName,
         "phone": phone,
@@ -55,40 +54,38 @@ export function AllBuildingsCard(props: IBuilding) {
         "zip": zip,
         "lat": lat,
         "lng": lng, 
-      })
+    })
     .then(() => {
       console.log("Building saved to user");
     })
     .catch((error) => {
       console.error("Error adding document: ", error);
     });
-  }
+  };
 
   const [/* modalState */, setModalState] = useContext(ModalContext);
   const handleShowLogin = () => setModalState(ModalState.LOGIN);
 
-  const mapViewUrl = `https://www.google.com/maps/search/?api=1&query=${streetNum}+${street}+${city}+${state}+${zip}`
+  const mapViewUrl = `https://www.google.com/maps/search/?api=1&query=${streetNum}+${street}+${city}+${state}+${zip}`;
 
   return (
     <div>
       <Card>
         <Card.Header>
           <Card.Title>
-            <h5 className="card-title"> {
-              <a id="buildingLink" 
-                href={urlForBuilding} 
-                target="_blank" 
-                rel="noreferrer">
-                {buildingName}
-              </a>
-            }</h5>
+            <a id="buildingLink" 
+              href={urlForBuilding} 
+              target="_blank" 
+              rel="noreferrer">
+              {buildingName}
+            </a>
           </Card.Title>
-          <h6 className="card-title">{residentialTargetedArea}</h6>
+          <h6>{residentialTargetedArea}</h6>
           { currentUser ? (
             buttonFill ?
               <Button 
                 variant="btn btn-info btn-sm btn-save-building-card"
-                onClick={ saveBuilding }
+                onClick={saveBuilding}
                 role="button">
                 Saved
               </Button>
@@ -101,7 +98,7 @@ export function AllBuildingsCard(props: IBuilding) {
               </Button>
             ) : (
             <>
-              <Button onClick={ handleShowLogin } variant="btn btn-outline-info btn-sm standalone-btn">
+              <Button onClick={handleShowLogin} variant="btn btn-outline-info btn-sm standalone-btn">
                 Save
               </Button>
             </>
@@ -119,15 +116,18 @@ export function AllBuildingsCard(props: IBuilding) {
               {city}, {state} {zip}
             </a>
             <br></br>
-            <a href="tel:{phone}">{phone}</a>
+            <a href="tel:{phone}">
+              {phone}
+            </a>
             { phone2 ? (
               <>
                 <br></br>
                 <a href="tel:{phone2}">
-                {phone2}</a>
+                  {phone2}
+                </a>
               </> 
               ) : ('') 
-              }
+            }
           </ListGroupItem>
           <ListGroupItem>
             Total MFTE: {totalRestrictedUnits}
@@ -146,4 +146,4 @@ export function AllBuildingsCard(props: IBuilding) {
       </Card>
     </div>
   );
-}
+};
