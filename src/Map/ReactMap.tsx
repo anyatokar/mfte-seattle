ef59d499556e3920dd3eae05f354d720f4601415
Matt McKnett <mmcknett@gmail.com>
2021-07-27 23:11:23 -0700

    Add markers and info windows to the ReactMap. There's still currently a bug where the info window is anchored to the same lat/long as the pin, instead of being anchored to the pin itself.

--- src/Map/ReactMap.tsx
+++ src/Map/ReactMap.tsx
@@ -1,5 +1,7 @@
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
-import IMap from "../interfaces/IMap";
+import { useState } from 'react';
+import { GoogleMap, InfoWindow, LoadScript, Marker } from '@react-google-maps/api';
+import IMap from '../interfaces/IMap';
+import IBuilding from '../interfaces/IBuilding';
 
 const containerStyle = {
   width: '100%',
@@ -12,6 +14,11 @@ const center = {
 };
 
 export function ReactMap(props: IMap) {
+  const { filteredBuildings = [] } = props;
+  const [selectedBuilding, setSelectedBuilding] = useState<IBuilding | null>(null);
+
+  const clearSelection = () => { setSelectedBuilding(null) };
+
   return (
     <LoadScript
       googleMapsApiKey={ `${process.env.REACT_APP_APIKEY}` }
@@ -24,8 +31,54 @@ export function ReactMap(props: IMap) {
         center={center}
         zoom={14}
       >
-        { /* Child components, such as markers, info windows, etc. */ }
-        <></>
+        <>
+          {
+            filteredBuildings.map(building => 
+              <Marker
+                animation={ google.maps.Animation.DROP }
+                position={{
+                  lat: building.lat,
+                  lng: building.lng
+                }}
+                onClick={() => setSelectedBuilding(building)}
+                key={ building.buildingID }
+              >
+                
+              </Marker>)
+          }
+          {
+            selectedBuilding &&
+            <InfoWindow
+              position={{
+                lat: selectedBuilding.lat,
+                lng: selectedBuilding.lng
+              }}
+              onCloseClick={clearSelection}
+            >
+              <>
+              <strong>
+                <a
+                  href={ selectedBuilding.urlForBuilding }
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  <br/>{ selectedBuilding.buildingName }
+                </a>
+              </strong>
+              <br/>
+              <strong>{ selectedBuilding.residentialTargetedArea }</strong>
+              <br/>
+              { selectedBuilding.streetNum } { selectedBuilding.street }
+              <br/>
+              { selectedBuilding.city }, { selectedBuilding.state } { selectedBuilding.zip }
+              <br/>
+              <br/>
+              <a href={ `tel:${selectedBuilding.phone}` }>{ selectedBuilding.phone }</a>
+              <br/>
+              </>
+            </InfoWindow>
+          }
+        </>
       </GoogleMap>
     </LoadScript>
   )
