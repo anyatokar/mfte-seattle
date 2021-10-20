31fba859125ceb07d2d0424a50726c3b942aa1ee
mmcknett <mmcknett@godaddy.com>
2021-10-19 22:44:22 -0700

    refactor: Extract useSavedBuildings custom hook

---
+++ src/hooks/useSavedBuildings.ts
@@ -0,0 +1,27 @@
+import { useCallback, useState, useEffect } from 'react';
+import firebase from '../db/firebase';
+import 'firebase/firestore';
+
+import IBuilding from "../interfaces/IBuilding";
+
+export function useSavedBuildings(currentUser: any) {
+  const ref = firebase.firestore().collection("users").doc(currentUser.uid).collection("savedHomes")
+  const [savedBuildings, setSavedBuildings] = useState([] as Array<IBuilding>);
+  const [loading, setLoading] = useState(false);
+
+  const getSavedBuildings = useCallback(() => {
+    setLoading(true) 
+    ref.onSnapshot((querySnapshot) => {
+    const items: Array<IBuilding> = [];
+    querySnapshot.forEach((doc) => {
+      items.push(doc.data() as IBuilding);
+    });
+    setSavedBuildings(items)
+    setLoading(false)
+    });
+  }, []);
+
+  useEffect(() => {getSavedBuildings()}, [getSavedBuildings]);
+
+  return [savedBuildings, loading];
+}
