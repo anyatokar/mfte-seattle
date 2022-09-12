9dea170a427c5b4e198f66d14f085aca4a48bfa0
Anya Tokar <61166946+anyatokar@users.noreply.github.com>
2022-09-11 22:31:27 -0400

    Update: Add disclaimer to homepage and minor formatting changes

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -21,18 +21,35 @@ const HomePage: React.FunctionComponent<IPage> = props => {
     <div className="jumbotron">
       <div className="container">
         <h1 className="display-5">MFTE Seattle</h1>
-        <p className="lead">
-          Find modern rent-reduced apartments in Seattle through the Multifamily Tax Exemption Program.
-        </p>
         <hr className="my-4"></hr>
         <p className="lead">
-        
+          Find modern rent-reduced apartments in Seattle through the Multifamily Tax Exemption program.
         </p>
+        <p className="lead">
+          This website is unaffiliated with the City of Seattle. Its intention is to help prospective MFTE tenants map the spreadsheet of properties provided by the City.
+        </p>
+        <ul className="resources-list lead">
+          <li>
+            <a id="MFTE_homepage"
+              href="https://www.seattle.gov/housing/renters/find-housing#multifamilytaxexemptionmfteincentivezoning"
+              target="_blank"
+              rel="noreferrer">
+              City of Seattle MFTE homepage
+            </a>
+          </li>
+          <li>
+            <a id="spreadsheet_of_properties: https://www.seattle.gov/Documents/Departments/Housing/HousingDevelopers/MultifamilyTaxExemption/MFTEParticipantContact.pdf"
+              href="https://www.seattle.gov/Documents/Departments/Housing/HousingDevelopers/MultifamilyTaxExemption/MFTEParticipantContact.pdf"
+              target="_blank"
+              rel="noreferrer">
+              Spreadsheet of properties
+            </a>
+          </li>
+        </ul>
+        <hr className="my-4"></hr>
         <div className="btn-toolbar">
           <Button onClick={onClick} value="./buildings" variant="outline-info" className="btn-lg standalone-btn">View Buildings</Button>
           <Button onClick={onClick} value="./about" variant="outline-info" className="btn-lg standalone-btn">About MFTE</Button>
-          {/* <a className="btn btn-outline-info btn-lg standalone-btn" href="./buildings" role="button">View Buildings</a>
-          <a className="btn btn-outline-info btn-lg standalone-btn" href="./about" role="button">About MFTE</a> */}
         </div>
       </div>
     </div>
