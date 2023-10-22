aac7be77add753c702770ac3cddcb333087b4aed
Matt McKnett <mmcknett@gmail.com>
2023-10-22 13:48:46 -0700

    fix: Put `firebaseConfig` directly into source.
    These constants aren't actually secrets. See https://firebase.google.com/support/guides/security-checklist#understand_api_keys

--- src/db/firebase.ts
+++ src/db/firebase.ts
@@ -3,14 +3,14 @@ import "firebase/database"
 import "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_APIKEY,
-  authDomain: process.env.REACT_APP_AUTHDOMAIN,
-  databaseURL: process.env.REACT_APP_DB,
-  projectId: process.env.REACT_APP_PID,
-  storageBucket: process.env.REACT_APP_SB,
-  messagingSenderId: process.env.REACT_APP_SID,
-  appId: process.env.REACT_APP_APPID,
-  measurementId: process.env.REACT_APP_MID
+  apiKey: "AIzaSyCtmQ3YXaf4KxHfztDl2846q8Galz2HOuk",
+  authDomain: "mfte-simple-92c08.firebaseapp.com",
+  databaseURL: "https://mfte-simple-92c08-default-rtdb.firebaseio.com",
+  projectId: "mfte-simple-92c08",
+  storageBucket: "mfte-simple-92c08.appspot.com",
+  messagingSenderId: "594673713013",
+  appId: "1:594673713013:web:e6e60f494477b92a18dd3c",
+  measurementId: "G-MSQZT5PQH1"
 };
 
 const app = firebase.initializeApp(firebaseConfig);
