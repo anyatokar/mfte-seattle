32d705aa3a642bc108ba74a5d4c339f65051557c
Anya Tokar <61166946+anyatokar@users.noreply.github.com>
2023-10-24 13:32:35 -0400

    Update node, react, clean up dependencies (#45)
    * bumped google-map-react dependency to ^2.1.9 - this got lost in a merge conflict
    
    * bumped react and react-dom - otherwise components were invalid JSX elements
    
    * root.render update based on react 18 documentation
    
    * bumped up to node 18 (from 16), had to also bump react scripts. getting a warning in the console people on the internet say to ignore - app starts and builds successfully
    
    * installed @babel/plugin-proposal-private-property-in-object: ^7.21.11 - suggestion on yarn start
    
    * saved missing dep @firebase/util
    
    * added eslint-config-react-app: ^7.0.1
    
    * removed styled components, don't see issues on yarn start/build
    
    * removed react-firebase/database and firestore-export-import
    
    * removed quite a few unused deps
    
    * removed quite a few unused dev deps

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './app';
 import reportWebVitals from './reportWebVitals';
 
-render(
+const container = document.getElementById('root')
+const root = createRoot(container!);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
