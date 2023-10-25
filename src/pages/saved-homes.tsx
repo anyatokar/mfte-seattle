666e19fa5583c51d2181082015b73a12692b0ab5
Anya Tokar <61166946+anyatokar@users.noreply.github.com>
2023-10-25 04:13:48 -0400

    Fix: fixed footer on rest of pills pages

--- src/pages/saved-homes.tsx
+++ src/pages/saved-homes.tsx
@@ -17,56 +17,54 @@ const SavedByUserPage: React.FunctionComponent<IPage & RouteComponentProps<any>>
   }
 
   return (
-    <div>
-      <Container fluid className="pills-page">
-        <Tab.Container id="sidebar" defaultActiveKey="list">
-          <Row>
-            <Col sm={2}>
-              <Nav variant="pills" className="flex-column">
-                <Nav.Item>
-                  <Nav.Link eventKey="map" className="tab">Map</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey="list" className="tab">List</Nav.Link>
-                </Nav.Item>
-              </Nav>
-            </Col>
-            <Col sm={10}>
-              <Tab.Content>
-                <Row>
-                  {/* top margin size 3 for all screens (xs and up) | top margin size of 0 for medium screens and up */}
-                  <Col className="mt-3 mt-md-0">
-                    <p className="lead">Saved buildings — your short list of apartment buildings.</p>
-                    <p>The list and notes are private to your profile.</p>
-                  </Col>
-                </Row>
-                {loading && <Spinner animation="border" variant="warning" />}
-                {!loading && savedBuildings.length === 0 &&
-                  <>
-                    <br></br>
-                    <p>Empty for now! To start your list, use the Save button in the&nbsp;
-                      <a id="Buildings_tab"
-                        href="./Buildings"
-                        title="View the map of MFTE properties">
-                        MFTE Map
-                      </a>&nbsp;tab.
-                    </p>
-                  </>}
-                <Tab.Pane eventKey="map">
-                  <MapTab
-                    buildingsToMap={savedBuildings}
-                    savedBuildings={savedBuildings}
-                  />
-                </Tab.Pane>
-                <Tab.Pane eventKey="list">
-                  <SavedHomesList savedBuildings={savedBuildings}/>
-                </Tab.Pane>
-              </Tab.Content>
-            </Col>
-          </Row>
-        </Tab.Container>
-      </Container>
-    </div>
+    <Container fluid className="pills-page">
+      <Tab.Container id="sidebar" defaultActiveKey="list">
+        <Row>
+          <Col sm={2}>
+            <Nav variant="pills" className="flex-column">
+              <Nav.Item>
+                <Nav.Link eventKey="map" className="tab">Map</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="list" className="tab">List</Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Col>
+          <Col sm={10}>
+            <Tab.Content>
+              <Row>
+                {/* top margin size 3 for all screens (xs and up) | top margin size of 0 for medium screens and up */}
+                <Col className="mt-3 mt-md-0">
+                  <p className="lead">Saved buildings — your short list of apartment buildings.</p>
+                  <p>The list and notes are private to your profile.</p>
+                </Col>
+              </Row>
+              {loading && <Spinner animation="border" variant="warning" />}
+              {!loading && savedBuildings.length === 0 &&
+                <>
+                  <br></br>
+                  <p>Empty for now! To start your list, use the Save button in the&nbsp;
+                    <a id="Buildings_tab"
+                      href="./Buildings"
+                      title="View the map of MFTE properties">
+                      MFTE Map
+                    </a>&nbsp;tab.
+                  </p>
+                </>}
+              <Tab.Pane eventKey="map">
+                <MapTab
+                  buildingsToMap={savedBuildings}
+                  savedBuildings={savedBuildings}
+                />
+              </Tab.Pane>
+              <Tab.Pane eventKey="list">
+                <SavedHomesList savedBuildings={savedBuildings}/>
+              </Tab.Pane>
+            </Tab.Content>
+          </Col>
+        </Row>
+      </Tab.Container>
+    </Container>
   )
 }
 
