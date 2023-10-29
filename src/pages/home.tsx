e9fbccc6b6ab32329cdfd4710ca593d84e720657
Anya Tokar <61166946+anyatokar@users.noreply.github.com>
2023-10-29 16:54:23 -0400

    removed a button from home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import IPage from '../interfaces/IPage';
 import logging from '../config/logging';
 import { useHistory } from "react-router-dom";
-import { Button, Container, Stack } from "react-bootstrap"
+import { Button, Container } from "react-bootstrap";
 
 const HomePage: React.FunctionComponent<IPage> = props => {
   useEffect(() => {
@@ -47,22 +47,13 @@ const HomePage: React.FunctionComponent<IPage> = props => {
       </p>
       <p className="lead"> Contact buildings directly for current availability.</p>
       <hr className="my-4"></hr>
-      <Stack gap={2}>
-        <Button
-          className="btn-lg col-lg-4 col-xl-3"
-          variant="outline-info"
-          onClick={onClick}
-          value="./buildings">
-          View Buildings Map
-        </Button>
-        <Button
-          className="btn-lg col-lg-4 col-xl-3"
-          variant="outline-info"
-          onClick={onClick}
-          value="./about">
-          About MFTE Seattle
-        </Button>
-      </Stack>
+      <Button
+        className="btn-lg col-lg-4 col-xl-3"
+        variant="outline-info"
+        onClick={onClick}
+        value="./buildings">
+        View Buildings Map
+      </Button>
     </Container>
   )
 }
