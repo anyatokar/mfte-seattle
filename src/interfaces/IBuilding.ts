51ca8f47f2ca94bf9138149e407fb30a6471a39d
Anya Tokar <61166946+anyatokar@users.noreply.github.com>
2023-11-06 14:51:47 -0500

    don't show apt type in table if there are 0 units of that apt type

--- src/interfaces/IBuilding.ts
+++ src/interfaces/IBuilding.ts
@@ -4,12 +4,12 @@ export default interface IBuilding {
   phone: string;
   phone2: string;
   residentialTargetedArea: string;
-  totalRestrictedUnits: string;
-  sedu: string;
-  studioUnits: string;
-  oneBedroomUnits: string;
-  twoBedroomUnits: string;
-  threePlusBedroomUnits: string;
+  totalRestrictedUnits: 0 | string;
+  sedu: 0 | string;
+  studioUnits: 0 | string;
+  oneBedroomUnits: 0 | string;
+  twoBedroomUnits: 0 | string;
+  threePlusBedroomUnits: 0 | string;
   urlForBuilding: string;
   lat: number;
   lng: number;
