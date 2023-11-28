4be2af2f776ce105b22c8dc895d483348570fef3
Anya Tokar <61166946+anyatokar@users.noreply.github.com>
2023-11-28 14:37:10 -0500

    tiny: code cleanup

--- src/auth_components/Profile.tsx
+++ src/auth_components/Profile.tsx
@@ -1,30 +1,31 @@
-import { Card } from "react-bootstrap";
+import {  Container, Row, Card, Col } from "react-bootstrap";
 import { useAuth } from "../contexts/AuthContext";
-import { Container, Row, Col } from "react-bootstrap";
 
 export default function Profile() {
   const { currentUser } = useAuth() as any;
 
+  if (!currentUser) {
+    return null;
+  }
+
   return (
-    currentUser && (
-      <Container fluid>
-        <Row>
-          <Col lg={6}>
-            <Card>
-              <Card.Body>
-                <Card.Text>
-                  <strong>Name: </strong>
-                  {currentUser.displayName}
-                </Card.Text>
-                <Card.Text>
-                  <strong>Email: </strong>
-                  {currentUser.email}
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
-    )
+    <Container fluid>
+      <Row>
+        <Col lg={6}>
+          <Card>
+            <Card.Body>
+              <Card.Text>
+                <strong>Name: </strong>
+                {currentUser.displayName}
+              </Card.Text>
+              <Card.Text>
+                <strong>Email: </strong>
+                {currentUser.email}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 }
