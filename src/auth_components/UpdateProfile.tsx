c4caaeb9b295a77d81f5e924fbdd026d06aa0609
Anya Tokar <61166946+anyatokar@users.noreply.github.com>
2023-11-30 16:04:39 -0500

    fixed bug where if name doesn't exist in Auth, there's no form field to update it

--- src/auth_components/UpdateProfile.tsx
+++ src/auth_components/UpdateProfile.tsx
@@ -84,7 +84,7 @@ export default function UpdateProfile() {
       .then(() => {
         // Auth is the source of truth for name/email/password
         // Firestore stores name/email as well but it's still a
-        // Success for the user if Firestore update fails (unlikely)
+        // Success for the user if Firestore update fails
         setMessage("Success! Account updated.");
         console.log("Account updated in Auth.");
 
@@ -92,8 +92,8 @@ export default function UpdateProfile() {
           .then(() => {
             console.log("Account updated in Firestore.");
           })
-          .catch(() => {
-            console.log("Error updating account in Firestore.");
+          .catch((error) => {
+            console.log(`Error updating account in Firestore: ${error.code}, ${error.message}`);
           });
       })
       .catch((error) => {
@@ -147,18 +147,16 @@ export default function UpdateProfile() {
               )}
 
               <Form onSubmit={handleFormSubmit}>
-                {currentUser.displayName && (
-                  <Form.Group id="displayName" className="form-group">
-                    <Form.Label>Name</Form.Label>
-                    <Form.Control
-                      required
-                      type="displayName"
-                      ref={displayNameRef}
-                      defaultValue={currentUser.displayName}
-                      onChange={handleChange}
-                    />
-                  </Form.Group>
-                )}
+                <Form.Group id="displayName" className="form-group">
+                  <Form.Label>Name</Form.Label>
+                  <Form.Control
+                    required
+                    type="displayName"
+                    ref={displayNameRef}
+                    defaultValue={currentUser.displayName}
+                    onChange={handleChange}
+                  />
+                </Form.Group>
                 <Form.Group id="email" className="form-group">
                   <Form.Label>Email</Form.Label>
                   <Form.Control
