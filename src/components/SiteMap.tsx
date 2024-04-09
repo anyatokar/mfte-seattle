6d965d2c6b65d7df773f2742d2389410c857b915
Anya Tokar <61166946+anyatokar@users.noreply.github.com>
2024-04-09 17:08:06 -0400

    ran prettier

--- src/components/SiteMap.tsx
+++ src/components/SiteMap.tsx
@@ -4,31 +4,33 @@ import { Link } from "react-router-dom"; // Assuming you are using React Router 
 const SiteMap = () => {
   return (
     <Nav className="justify-content-center">
-      <Nav.Item className="px-3 px-md-4  pt-3">
+      <Nav.Item className="px-3 px-md-4 pt-3">
         <Link className="footer-nav-link-custom" to="/all-buildings">
           MFTE Map
         </Link>
       </Nav.Item>
       {/* <Nav.Item className="p-1">| </Nav.Item> */}
-      <Nav.Item className="px-3  px-md-4 pt-3">
+      <Nav.Item className="px-3 px-md-4 pt-3">
         <Link className="footer-nav-link-custom" to="/resources">
           Resources
         </Link>
       </Nav.Item>
       {/* <Nav.Item className="p-1">| </Nav.Item> */}
-      <Nav.Item className="px-3  px-md-4 pt-3">
+      <Nav.Item className="px-3 px-md-4 pt-3">
         <Link className="footer-nav-link-custom" to="/contact">
           Contact Us
         </Link>
       </Nav.Item>
       {/* <Nav.Item className="p-1">| </Nav.Item> */}
-      <Nav.Item className="px-3   px-md-4  pt-3">
+      <Nav.Item className="px-3 px-md-4 pt-3">
         <Link className="footer-nav-link-custom" to="/privacy-policy">
           Privacy Policy
         </Link>
       </Nav.Item>
       {/* <Nav.Item className="p-1">| </Nav.Item> */}
-      <Nav.Item className="px-3  px-md-4 pt-3">Copyright © 2021-2024 Anya Tokar</Nav.Item>
+      <Nav.Item className="px-3 px-md-4 pt-3">
+        Copyright © 2021-2024 Anya Tokar
+      </Nav.Item>
     </Nav>
   );
 };
