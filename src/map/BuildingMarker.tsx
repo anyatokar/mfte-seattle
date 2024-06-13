1846f5730f6e9d7aede9dc6e448d1cd53717e467
Anya Tokar <61166946+anyatokar@users.noreply.github.com>
2024-06-13 16:59:23 -0400

    Tiny: pin color change (#94)

--- src/map/BuildingMarker.tsx
+++ src/map/BuildingMarker.tsx
@@ -72,7 +72,7 @@ export function BuildingMarker(props: IBuildingMarkerProps) {
 
   const svgMarkerNoAd = {
     path: "M0 0q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
-    fillColor: "blue",
+    fillColor: "#10345c",
     strokeColor: "black",
     strokeWeight: 1,
     fillOpacity: 0.8,
