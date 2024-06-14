f77248de3cbeafb7d363d2160fab989a7f0eff44
Anya Tokar <61166946+anyatokar@users.noreply.github.com>
2024-06-13 22:37:43 -0400

    fix small bug where renter had to be logged in to see listings

--- src/hooks/useAllListings.ts
+++ src/hooks/useAllListings.ts
@@ -13,8 +13,6 @@ export function useAllListings(): [IListing[], boolean] {
   const { currentUser } = useAuth();
 
   const getAllListings = useCallback(() => {
-    if (!currentUser) return;
-
     setLoadingAllListings(true);
 
     const q = query(collection(db, "listings"));
