56a94a9b2310b1283337f2ccc2fc8d23d1b757ee
Anya Tokar <61166946+anyatokar@users.noreply.github.com>
2024-09-08 21:42:21 -0400

    tiny: removed commented out import

--- src/components/BuildingContactInfo.test.js
+++ src/components/BuildingContactInfo.test.js
@@ -1,5 +1,4 @@
 import { render, screen } from "@testing-library/react";
-// import '@testing-library/jest-dom/extend-expect'; // For extended matchers
 import { BuildingName, AddressAndPhone } from "./BuildingContactInfo";
 
 describe("BuildingName Component", () => {
