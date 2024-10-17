1808a3442798ab56f4065c69222df4fbcbfe540a
Anya Tokar <61166946+anyatokar@users.noreply.github.com>
2024-10-17 12:04:35 -0400

    updated button labels on are you sure modal

--- src/components/AreYouSureModal.tsx
+++ src/components/AreYouSureModal.tsx
@@ -18,20 +18,28 @@ const AreYouSureModal: React.FunctionComponent<AreYouSureModalProps> = ({
   type ModalText = {
     header: string;
     body: string;
+    goBackLabel: string;
+    confirmLabel: string;
   };
 
   const modalTextLookup: { [key in confirmModalTypeEnum]: ModalText } = {
     [confirmModalTypeEnum.LISTING_CANCEL_EDIT]: {
       header: "Confirm Cancel",
       body: "Are you sure you want to cancel any unsaved changes?",
+      goBackLabel: "Go Back",
+      confirmLabel: "Confirm",
     },
     [confirmModalTypeEnum.LISTING_DELETE]: {
       header: "Confirm Delete",
       body: "Are you sure you want to delete this listing? This can't be undone. You may choose to archive it instead.",
+      goBackLabel: "Cancel",
+      confirmLabel: "Delete",
     },
     [confirmModalTypeEnum.ACCOUNT_DELETE]: {
       header: "Confirm Delete Account",
       body: "Are you sure you want to delete your account? This can't be undone.",
+      goBackLabel: "Cancel",
+      confirmLabel: "Delete",
     },
   };
 
@@ -49,10 +57,10 @@ const AreYouSureModal: React.FunctionComponent<AreYouSureModalProps> = ({
       <Modal.Body>{modalText?.body}</Modal.Body>
       <Modal.Footer>
         <Button variant="secondary" onClick={handleClose}>
-          Go Back
+          {modalText?.goBackLabel || "Go Back"}
         </Button>
         <Button variant="danger" onClick={handleConfirm}>
-          Confirm
+          {modalText?.confirmLabel || "Confirm"}
         </Button>
       </Modal.Footer>
     </Modal>
