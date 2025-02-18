61046a0de981bbad487b9cb4ae9138fc94ee6289
Anya Tokar <61166946+anyatokar@users.noreply.github.com>
2025-02-18 14:28:52 -0500

    remove unnecessary arrow function

--- src/components/BedroomCheckbox.tsx
+++ src/components/BedroomCheckbox.tsx
@@ -25,7 +25,7 @@ const BedroomCheckbox: React.FC<BedroomCheckboxProps> = ({
       id={checkboxKey}
       value={checkboxKey}
       checked={isChecked}
-      onChange={() => handleCheck()}
+      onChange={handleCheck}
     />
   );
 };
