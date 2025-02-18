256ecbf3567004e631d90fa54e8784334da0e454
Anya Tokar <61166946+anyatokar@users.noreply.github.com>
2025-02-18 10:09:14 -0500

    filtering logic

--- src/utils/buildingsFilter.ts
+++ src/utils/buildingsFilter.ts
@@ -1,23 +1,53 @@
 import IBuilding from "../interfaces/IBuilding";
+import { UnitSize } from "../interfaces/IListing";
 import { BedroomsKeyEnum } from "../types/enumTypes";
 
 export type ActiveFilters = {
   bedrooms: Set<BedroomsKeyEnum>;
   neighborhoods: Set<string>;
+  isAvailOnly: boolean;
 };
+
+function filterBedrooms(
+  building: IBuilding,
+  activeFilters: ActiveFilters
+): boolean {
+  const { bedrooms, isAvailOnly } = activeFilters;
+
+  // If no filters are applied, return true.
+  if (bedrooms.size === 0) return true;
+
+  const unitSizeToBedroomsKey: Record<UnitSize, BedroomsKeyEnum> = {
+    micro: BedroomsKeyEnum.SEDU,
+    studio: BedroomsKeyEnum.STUDIO,
+    oneBed: BedroomsKeyEnum.ONE_BED,
+    twoBed: BedroomsKeyEnum.TWO_BED,
+    threePlusBed: BedroomsKeyEnum.THREE_PLUS,
+  };
+
+  if (isAvailOnly) {
+    return (
+      building.listing?.availData.some((unit) =>
+        bedrooms.has(unitSizeToBedroomsKey[unit.unitSize])
+      ) ?? false
+    );
+  }
+
+  return [...bedrooms].some((filterProperty) =>
+    Boolean(building[filterProperty])
+  );
+}
 
 export function buildingsFilter(
   building: IBuilding,
   activeFilters: ActiveFilters
 ): boolean {
-  const { bedrooms, neighborhoods } = activeFilters;
+  const { neighborhoods } = activeFilters;
 
-  // If no boxes are checked it's evaluated to be the same as the box is checked, basically omits that dropdown.
-  const bedroomsResult =
-    activeFilters.bedrooms.size === 0 ||
-    [...bedrooms].some((filterProperty) => building[filterProperty]);
+  const bedroomsResult = filterBedrooms(building, activeFilters);
 
   const neighborhoodsResult =
+    // If no boxes are checked, evaluate to be the same as the box is checked - omit that dropdown.
     activeFilters.neighborhoods.size === 0 ||
     neighborhoods.has(building.residentialTargetedArea);
 
