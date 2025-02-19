a8d159156c77fc260b2fc0aa1ee0512da7c8de40
Anya Tokar <61166946+anyatokar@users.noreply.github.com>
2025-02-19 17:32:01 -0500

    updated legend

--- src/map/Legend.tsx
+++ src/map/Legend.tsx
@@ -3,12 +3,16 @@ const Legend: React.FC = () => {
     <div style={legendStyles}>
       <h6 className="visually-hidden">Legend</h6>
       <div style={legendItemStyle}>
-        <span style={colorBox(true)}></span>
+        <span style={colorBox("hasListing")}></span>
         <span>MFTE units available</span>
       </div>
       <div style={legendItemStyle}>
-        <span style={colorBox(false)}></span>
+        <span style={colorBox("availUnknown")}></span>
         <span>Unknown availability</span>
+      </div>
+      <div style={legendItemStyle}>
+        <span style={colorBox("saved")}></span>
+        <span>Saved</span>
       </div>
     </div>
   );
@@ -18,7 +22,6 @@ const legendStyles: React.CSSProperties = {
   background: "white",
   padding: "7px",
   margin: "3px",
-  border: "1px solid #ccc",
   fontSize: "14px",
   boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
   borderRadius: "5px",
@@ -32,14 +35,21 @@ const legendItemStyle: React.CSSProperties = {
   gap: "2px",
 };
 
-const colorBox = (hasListing: boolean): React.CSSProperties => ({
+const colorBox = (
+  type: "hasListing" | "availUnknown" | "saved"
+): React.CSSProperties => ({
   width: "15px",
   height: "15px",
-  backgroundColor: hasListing ? "red" : "#10345c",
+  backgroundColor:
+    type === "hasListing"
+      ? "red"
+      : type === "availUnknown"
+        ? "#10345c"
+        : "white",
   marginRight: "5px",
-  opacity: hasListing ? 1 : 0.8,
-  border: "1px solid black",
+  opacity: type === "hasListing" ? 1 : 0.8,
   borderRadius: "4px",
+  border: type === "saved" ? "2px solid black" : "none",
 });
 
 export default Legend;
