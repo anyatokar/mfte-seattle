7942d3e447da7ec778eb19b52e6f843b33c001ef
Anya Tokar <61166946+anyatokar@users.noreply.github.com>
2025-02-19 21:19:13 -0500

    created mobile and desktop views for filters

--- src/components/SearchAndFilter.tsx
+++ src/components/SearchAndFilter.tsx
@@ -1,7 +1,5 @@
 import { Dispatch, useContext, useState } from "react";
 import SearchInput from "./SearchInput";
-import BedroomCheckbox from "./checkboxes/BedroomCheckbox";
-import FilterCheckbox from "./checkboxes/FilterCheckbox";
 import FilterSwitch from "./checkboxes/FilterSwitch";
 import { ActiveFilters } from "../utils/buildingsFilter";
 import { FilterAction } from "../reducers/filterReducer";
@@ -9,13 +7,12 @@ import { BedroomsKeyEnum } from "../types/enumTypes";
 import { ModalContext, ModalState } from "../contexts/ModalContext";
 import { useAuth } from "../contexts/AuthContext";
 
-import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
-import Dropdown from "react-bootstrap/Dropdown";
-import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import Stack from "react-bootstrap/Stack";
+import BedroomDropdown from "./searchAndFilter/BedroomDropdown";
+import NeighborhoodDropdown from "./searchAndFilter/NeighborhoodDropdown";
 
 type SearchAndFilterProps = {
   setSearchQuery: React.Dispatch<React.SetStateAction<string>>;
@@ -26,7 +23,7 @@ type SearchAndFilterProps = {
 
 const SearchAndFilter: React.FC<SearchAndFilterProps> = ({
   setSearchQuery,
-  allNeighborhoods: neighborhoods,
+  allNeighborhoods,
   activeFilters,
   dispatch,
 }) => {
@@ -63,96 +60,49 @@ const SearchAndFilter: React.FC<SearchAndFilterProps> = ({
     }
   };
 
-  const [dropdownFilter, setDropdownFilter] = useState("");
-
-  const filteredNeighborhoods = [...neighborhoods]
-    .filter((neighborhood) =>
-      neighborhood.toLowerCase().includes(dropdownFilter.toLowerCase())
-    )
-    .sort();
-
-  const checkboxKeys: BedroomsKeyEnum[] = [
-    BedroomsKeyEnum.SEDU,
-    BedroomsKeyEnum.STUDIO,
-    BedroomsKeyEnum.ONE_BED,
-    BedroomsKeyEnum.TWO_BED,
-    BedroomsKeyEnum.THREE_PLUS,
-  ];
-
   return (
     <Container fluid>
-      <Row className="p-0 mt-0 mb-2">
+      <Row className="p-0 mt-0 mb-1">
         {/* search */}
-        <Col sm={8} lg={4} className="mb-2 mb-sm-0">
+        <Col sm={7} md={6} lg={4} className="mb-md-0">
           <SearchInput setSearchQuery={(query) => setSearchQuery(query)} />
         </Col>
 
-        {/* filter */}
-        <Col>
+        {/* filter for large screens */}
+        <Col className="mb-1 d-none d-md-block">
           <Stack direction="horizontal" gap={2}>
-            <Dropdown>
-              <Dropdown.Toggle
-                className="diy-solid-info-button "
-                id="bedroom-filter-dropdown"
-                size="sm"
-              >
-                Bedrooms
-              </Dropdown.Toggle>
-              <Dropdown.Menu
-                className="p-2"
-                aria-labelledby="bedroom-filter-dropdown"
-              >
-                {checkboxKeys.map((checkboxKey) => (
-                  <Form key={checkboxKey}>
-                    <BedroomCheckbox
-                      checkboxKey={checkboxKey}
-                      onCheckboxChange={handleBedroomsChange}
-                    />
-                  </Form>
-                ))}
-              </Dropdown.Menu>
-            </Dropdown>
+            <BedroomDropdown onBedroomsChange={handleBedroomsChange} />
+            <NeighborhoodDropdown
+              onNeighborhoodsChange={handleNeighborhoodsChange}
+              allNeighborhoods={allNeighborhoods}
+              activeFilters={activeFilters}
+            />
+            <FilterSwitch
+              onCheckboxChange={handleAvailOnlyToggle}
+              type="knownOnly"
+            />
+            <FilterSwitch
+              onCheckboxChange={handleSavedOnlyToggle}
+              type="savedOnly"
+            />
+          </Stack>
+        </Col>
+      </Row>
 
-            <Dropdown>
-              <Dropdown.Toggle
-                className="diy-solid-info-button"
-                id="neighborhood-filter-dropdown"
-                size="sm"
-              >
-                Neighborhood
-              </Dropdown.Toggle>
-              <Dropdown.Menu
-                style={{ maxHeight: "200px", overflowY: "auto" }}
-                className="p-2"
-                aria-labelledby="neighborhood-filter-dropdown"
-              >
-                <Form>
-                  <Form.Control
-                    type="search"
-                    aria-label="Filter neighborhoods"
-                    placeholder="Filter list"
-                    onChange={(e) => setDropdownFilter(e.target.value)}
-                    value={dropdownFilter}
-                    size="sm"
-                  />
-                  <Button
-                    variant="link"
-                    onClick={() => handleNeighborhoodsChange()}
-                  >
-                    Remove Filter
-                  </Button>
-                  {[...filteredNeighborhoods].map((neighborhood) => (
-                    <FilterCheckbox
-                      key={neighborhood}
-                      checkboxKey={neighborhood}
-                      onCheckboxChange={handleNeighborhoodsChange}
-                      isChecked={activeFilters.neighborhoods.has(neighborhood)}
-                    />
-                  ))}
-                </Form>
-              </Dropdown.Menu>
-            </Dropdown>
-
+      {/* filter for small screens */}
+      <Row className="d-md-none">
+        <Col sm={5} className="mb-1">
+          <Stack direction="horizontal" gap={2}>
+            <BedroomDropdown onBedroomsChange={handleBedroomsChange} />
+            <NeighborhoodDropdown
+              onNeighborhoodsChange={handleNeighborhoodsChange}
+              allNeighborhoods={allNeighborhoods}
+              activeFilters={activeFilters}
+            />
+          </Stack>
+        </Col>
+        <Col sm={6} className="mb-1">
+          <Stack direction="horizontal" gap={2}>
             <FilterSwitch
               onCheckboxChange={handleAvailOnlyToggle}
               type="knownOnly"
---
+++ src/components/searchAndFilter/NeighborhoodDropdown.tsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import FilterCheckbox from "../checkboxes/FilterCheckbox";
+import { ActiveFilters } from "../../utils/buildingsFilter";
+
+import Dropdown from "react-bootstrap/Dropdown";
+import Button from "react-bootstrap/esm/Button";
+import Form from "react-bootstrap/Form";
+
+type NeighborhoodFilterProps = {
+  onNeighborhoodsChange: (checkbox?: string) => void;
+  allNeighborhoods: Set<string>;
+  activeFilters: ActiveFilters;
+};
+
+const NeighborhoodDropdown: React.FC<NeighborhoodFilterProps> = ({
+  onNeighborhoodsChange,
+  allNeighborhoods,
+  activeFilters,
+}) => {
+  const [dropdownFilter, setDropdownFilter] = useState("");
+  const filteredNeighborhoods = [...allNeighborhoods]
+    .filter((neighborhood) =>
+      neighborhood.toLowerCase().includes(dropdownFilter.toLowerCase())
+    )
+    .sort();
+  return (
+    <Dropdown>
+      <Dropdown.Toggle
+        className="diy-solid-info-button"
+        id="neighborhood-filter-dropdown"
+        size="sm"
+      >
+        Neighborhood
+      </Dropdown.Toggle>
+      <Dropdown.Menu
+        style={{ maxHeight: "200px", overflowY: "auto" }}
+        className="p-2"
+        aria-labelledby="neighborhood-filter-dropdown"
+      >
+        <Form>
+          <Form.Control
+            type="search"
+            aria-label="Filter neighborhoods"
+            placeholder="Filter list"
+            onChange={(e) => setDropdownFilter(e.target.value)}
+            value={dropdownFilter}
+            size="sm"
+          />
+          <Button variant="link" onClick={() => onNeighborhoodsChange()}>
+            Remove Filter
+          </Button>
+          {[...filteredNeighborhoods].map((neighborhood) => (
+            <FilterCheckbox
+              key={neighborhood}
+              checkboxKey={neighborhood}
+              onCheckboxChange={onNeighborhoodsChange}
+              isChecked={activeFilters.neighborhoods.has(neighborhood)}
+            />
+          ))}
+        </Form>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+};
+
+export default NeighborhoodDropdown;
