775291b9812f4e0ec7daa5c968e6458050c15ae5
Anya Tokar <61166946+anyatokar@users.noreply.github.com>
2025-07-09 12:03:34 -0400

    fixed PWA - vite-plugin-pwa installation and setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,45 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        cleanupOutdatedCaches: true,
+      },
+      manifest: {
+        short_name: "MFTE Seattle",
+        name: "MFTE Seattle - Find Rent-Reduced Apartments",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#e2fbfd",
+        theme_color: "#0f3b47",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      includeAssets: [
+        "favicon.ico",
+        "android-chrome-192x192.png",
+        "android-chrome-512x512.png",
+        "apple-touch-icon.png",
+      ],
+    }),
+  ],
   server: {
     open: true,
     port: 8080,
