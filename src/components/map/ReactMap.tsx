1a08f12ddaa5c4238b849c85309150a440a50f23
Anya Tokar <61166946+anyatokar@users.noreply.github.com>
2025-07-25 14:56:29 -0400

    map state was needed for map buttons

--- src/components/map/ReactMap.tsx
+++ src/components/map/ReactMap.tsx
@@ -10,24 +10,7 @@ import IMap from "../../interfaces/IMap";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
-
-const seattle = {
-  lat: 47.62,
-  lng: -122.315,
-};
-
-const mapTypeControlOptions = window.google
-  ? {
-      style: window.google.maps.MapTypeControlStyle.DROPDOWN_MENU,
-      position: window.google.maps.ControlPosition.TOP_LEFT,
-      mapTypeIds: [
-        window.google.maps.MapTypeId.ROADMAP,
-        window.google.maps.MapTypeId.SATELLITE,
-        window.google.maps.MapTypeId.TERRAIN,
-        window.google.maps.MapTypeId.HYBRID,
-      ],
-    }
-  : undefined;
+import { useState } from "react";
 
 const ReactMap: React.FC<IMap> = ({
   resultBuildingsUnsorted = [],
@@ -37,44 +20,67 @@ const ReactMap: React.FC<IMap> = ({
   setSelectedBuildingId,
   selectedBuildingId,
 }) => {
+  const [isMapLoaded, setIsMapLoaded] = useState(false);
+
+  const seattleCoordinates = {
+    lat: 47.62,
+    lng: -122.315,
+  };
+
+  const mapTypeControlOptions = {
+    style: window.google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+    position: window.google.maps.ControlPosition.TOP_LEFT,
+    mapTypeIds: [
+      window.google.maps.MapTypeId.ROADMAP,
+      window.google.maps.MapTypeId.SATELLITE,
+      window.google.maps.MapTypeId.TERRAIN,
+      window.google.maps.MapTypeId.HYBRID,
+    ],
+  };
+
   return (
     <APIProvider
       apiKey={firebaseConfig.apiKey}
-      onLoad={() => console.log("Maps API has loaded.")}
+      onLoad={() => {
+        console.log("Maps API has loaded.");
+        setIsMapLoaded(true);
+      }}
     >
-      <Container fluid>
-        <Row>
-          <Col
-            className="p-0 map-and-list-container"
-            style={{ width: "100%", height: mapHeight }}
-          >
-            <Map
-              className="map-container"
-              defaultZoom={10.9}
-              defaultCenter={seattle}
-              mapId={"c8d48b060a22a457"}
-              onCameraChanged={(ev: MapCameraChangedEvent) =>
-                console.log(
-                  "camera changed:",
-                  ev.detail.center,
-                  "zoom:",
-                  ev.detail.zoom
-                )
-              }
-              mapTypeControl={true}
-              mapTypeControlOptions={mapTypeControlOptions}
+      {isMapLoaded && (
+        <Container fluid>
+          <Row>
+            <Col
+              className="p-0 map-and-list-container"
+              style={{ width: "100%", height: mapHeight }}
             >
-              <AllMarkers
-                buildingsToMap={resultBuildingsUnsorted}
-                shouldScroll={shouldScroll}
-                savedBuildings={savedBuildings}
-                setSelectedBuildingId={setSelectedBuildingId}
-                selectedBuildingId={selectedBuildingId}
-              />
-            </Map>
-          </Col>
-        </Row>
-      </Container>
+              <Map
+                className="map-container"
+                defaultZoom={10.9}
+                defaultCenter={seattleCoordinates}
+                mapId={"c8d48b060a22a457"}
+                onCameraChanged={(ev: MapCameraChangedEvent) =>
+                  console.log(
+                    "camera changed:",
+                    ev.detail.center,
+                    "zoom:",
+                    ev.detail.zoom
+                  )
+                }
+                mapTypeControl={true}
+                mapTypeControlOptions={mapTypeControlOptions}
+              >
+                <AllMarkers
+                  buildingsToMap={resultBuildingsUnsorted}
+                  shouldScroll={shouldScroll}
+                  savedBuildings={savedBuildings}
+                  setSelectedBuildingId={setSelectedBuildingId}
+                  selectedBuildingId={selectedBuildingId}
+                />
+              </Map>
+            </Col>
+          </Row>
+        </Container>
+      )}
     </APIProvider>
   );
 };
